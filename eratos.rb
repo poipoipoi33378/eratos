690292720310648ca4ce9aa1fa16672863293a79
菅原誠一 <poipoipoi33378@gmail.com>
2018-05-14 22:22:30 +0900

    1st　実装終了

--- eratos.rb
+++ eratos.rb
@@ -3,16 +3,43 @@ class Eratos
 
   def prime_number(max)
 
-    if max == 2 then
-      prime_number_list = [2]
-    elsif max == 3 then
-      prime_number_list = [2,3]
-    elsif max == 30 then
-      prime_number_list = [2,3,5,7,11,13,17,19,23,29]
-    else
-      prime_number_list = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+    #step1 検索リスト作成
+    serch_list = Array.new(max)
+    prime_number_list = Array.new
+    serch_list[0,1] = [false,false]
+    2.upto(max) do |i|
+      serch_list[i] = true
     end
+
+    #step2
+    # 探索リストの先頭の数を素数リストに移動し、その倍数を探索リストから篩い落とす
+    target = 0
+    while target <= max**(1/2.0) do
+      2.upto(max) do |i|
+        if serch_list[i] == true then
+          prime_number_list = prime_number_list + [i]
+          serch_list[i] = false
+          target = i
+          break
+        end
+      end
+      x = 1
+      while target*x <= max do
+        serch_list[target*x] = false
+        x = x + 1
+      end
+      #ステップ 3
+      #上記の篩い落とし操作を探索リストの先頭値がxの平方根に達するまで行う。
+    end
+
+    #ステップ 4
+    #探索リストに残った数を素数リストに移動して処理終了。
+    2.upto(max) do |i|
+      if serch_list[i] == true then
+        prime_number_list = prime_number_list + [i]
+      end
+    end
+
     return prime_number_list
   end
-
 end
