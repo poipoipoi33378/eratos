f16b56eeafa52eae7301b94437eda1c7a466e073
菅原誠一 <poipoipoi33378@gmail.com>
2018-05-14 20:53:40 +0900

    仮実装2,3,30,120

--- eratos.rb
+++ eratos.rb
@@ -2,12 +2,17 @@
 class Eratos
 
   def prime_number(max)
-    if max == 2 then
-      return [2]
-    else
-      return [2,3]
-    end
 
+    if max == 2 then
+      prime_number_list = [2]
+    elsif max == 3 then
+      prime_number_list = [2,3]
+    elsif max == 30 then
+      prime_number_list = [2,3,5,7,11,13,17,19,23,29]
+    else
+      prime_number_list = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+    end
+    return prime_number_list
   end
 
 end
--- spec/eratos_spec.rb
+++ spec/eratos_spec.rb
@@ -6,9 +6,19 @@ describe '#prime_number' do
     expect(eratos.prime_number(2)).to eq [2]
   end
 
-  it '2 3' do
+  it '3' do
     eratos = Eratos.new
     expect(eratos.prime_number(3)).to eq [2,3]
+  end
+
+  it '30' do
+    eratos = Eratos.new
+    expect(eratos.prime_number(30)).to eq [2,3,5,7,11,13,17,19,23,29]
+  end
+
+  it '120' do
+    eratos = Eratos.new
+    expect(eratos.prime_number(120)).to eq [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
   end
 
 end
